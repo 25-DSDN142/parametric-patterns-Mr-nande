3456e6e68b2441219614b2097d5a35a93f1ed7e4
nui <monihungrykids@gmmail.com>
2025-07-22 12:10:06 +1200

    tuesday class

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -18,9 +18,24 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   // background(240, 255, 240); //light honeydew green colour
-  background(0); //light honeydew green colour
+  background(74, 0, 255); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+ let size = 35;
+ let weight= 5;
+
+ fill(0,102,102);
+  strokeWeight(weight);
+  rect(50, 50, 50);
+  rect(100,50,50);
+
+  fill(211, 47, 47);
+  ellipse(75,75,size);
+  ellipse(125,75,size);
+
+
+
+
+
 }
