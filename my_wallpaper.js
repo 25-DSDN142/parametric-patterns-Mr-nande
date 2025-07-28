9a5006da6bffca55d781bee2cac1ce78ef1875b8
nui <monihungrykids@gmmail.com>
2025-07-29 10:18:16 +1200

    if statements

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
-var x = 100;
+
 
 
 function setup_wallpaper(pWallpaper) {
@@ -10,7 +10,7 @@ function setup_wallpaper(pWallpaper) {
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
-
+  
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
@@ -19,11 +19,26 @@ function setup_wallpaper(pWallpaper) {
 
 function wallpaper_background() {
   // background(240, 255, 240); //light honeydew green colour
-  background(74, 0, 255); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-//Rule of thirds
+  //Rule of thirds
+  let blue = color(74, 0, 255)
+  let red = color(255,0,0)
+
+  let BGC = red
+
+  background(BGC); //light honeydew green colour
+
+  if ( BGC == blue){
+    ellipse (100, 100, 50, 50);
+    ellipse( 95, 95, 40, 40);
+  }
+
+  if ( BGC == red){
+    rect(33, 33, 33);
+  }
+
   strokeWeight(0.25);
   line(0, 132, 200, 132);
   line(66, 0, 66, 200);
@@ -67,7 +82,7 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   endShape(CLOSE);
 
   //pasi pattern
-  strokeWeight(1);
+  strokeWeight(0.5);
   stroke(255, 255, 255);//white
   fill(0, 0, 0);//black
   beginShape();
@@ -83,5 +98,18 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   vertex(115.5, 50);
   vertex(99, 66);
   endShape(CLOSE);
+
+  //rect
+  strokeWeight(1);
+  stroke(31, 156, 35);//green
+  fill(6, 2, 112);//blue
+  rect(33, 99, 33);
+  rect(35, 101, 29);
+  rect(37, 103, 25);
+  rect(39, 105, 21);
+  rect(41, 107, 17);
+  rect(43, 109, 13);
+  rect(45, 111, 9);
+  rect(47, 113, 5);
 
 }
