94211e1ee4944fdbe0972dc4c03efe836ea2ba65
nui <monihungrykids@gmmail.com>
2025-07-30 17:26:31 +1200

    cloud with circle inside

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -19,43 +19,24 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-ellipse(99, 99, 50, 50);
-ellipse(99, 99, 40, 40);
-ellipse(99, 99, 30, 30);
-ellipse(99, 99, 20, 20);
-ellipse(99, 99, 10, 10);
+//cloud.lol
+//hmm..
+stroke("blue");
+fill("blue");
+ellipse(33, 66, 20, 20);
+ellipse(23, 70, 5, 5);
+ellipse(48, 56, 30, 30);
+ellipse(66, 50, 30, 30);
+ellipse(80, 56, 20, 20);
+ellipse(90, 66, 20, 20);
+ellipse(100, 76, 20, 10);
+ellipse(28, 76, 30, 10);
+ellipse(66, 74, 85, 25);
+ellipse(66, 63, 30, 10);
 
+//middle symbol ting
 strokeWeight(2);
-stroke(228, 8, 10);//red
-noFill();
-beginShape();
-vertex(33, 66);
-vertex(33, 99);
-vertex(66, 99);
-vertex(66, 66);
-vertex(48.5, 33);
-vertex(33, 66);
-endShape(CLOSE);
-
-
-strokeWeight(1);
-stroke(254, 153, 0);//orange
-fill(228, 50, 79);//pinky
-//head
-ellipse(170, 60, 35, 40);
-
-//hat.lol
-beginShape();
-vertex(140, 60);
-vertex(164, 55);
-vertex(192, 45);
-vertex(180, 40);
-vertex(170, 30);
-vertex(150, 40);
-vertex(150, 50);
-vertex(145, 50);
-endShape(CLOSE);
-
-//jolly roger
-ellipse
+stroke("white")
+fill("blue")
+ellipse(66, 66, 30, 30);
 }
